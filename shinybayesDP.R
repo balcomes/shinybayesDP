835d416cafc2e6c791cf76b53f793043b096967e
Shawn Balcome <balc0022@umn.edu>
2017-04-29 18:13:23 -0500

    Working prototype.

--- shinybayesDP.R
+++ shinybayesDP.R
@@ -1,29 +1,74 @@
-## app.R ##
+library(bayesDP)
+library(shiny)
 library(shinydashboard)
+library(shinythemes)
 
-ui <- dashboardPage(
-  dashboardHeader(title = "Basic dashboard"),
-  dashboardSidebar(),
+ui <- dashboardPage(title="bayesDP", 
+  dashboardHeader(title = "bayesDP"),
+  dashboardSidebar(
+    tags$head(tags$style(HTML(".sidebar{height:90vh;overflow-y:auto;}"))),
+    selectInput("func", 
+                "Select Function", 
+                choices = c("bdpnormal","bdpbinomial","bdpsurvival"), 
+                selected = "bdpnormal"),
+    uiOutput("params")
+  ),
   dashboardBody(
-    # Boxes need to be put in a row (or column)
-    fluidRow(
-      box(plotOutput("plot1", height = 250)),
-      
-      box(
-        title = "Controls",
-        sliderInput("slider", "Number of observations:", 1, 100, 50)
-      )
-    )
+    shinythemes::themeSelector(),
+    width = "100%",
+    height = "100%",
+    tabsetPanel(tabPanel("discount", "discount", plotOutput("discount")),
+                tabPanel("posteriors", "posteriors", plotOutput("posteriors")),
+                tabPanel("density", "density", plotOutput("density"))
+                ),
+    verbatimTextOutput("summary")
   )
 )
 
-server <- function(input, output) {
-  set.seed(122)
-  histdata <- rnorm(500)
+server <- function(input, output){
   
-  output$plot1 <- renderPlot({
-    data <- histdata[seq_len(input$slider)]
-    hist(data)
+  params <- reactive({
+    params <- as.list(args(input$func))
+    params[sapply(params, is.null)] <- 100
+    params <- params[-length(params)]
+    params
+    })
+  
+  params_names <- reactive({
+      names(params())
+    })
+  
+  output$params <- renderUI({
+    lapply(params_names(),function(x){
+      do.call(textInput,list(x, label = x, value = params()[[x]]))
+    })
+  })
+  
+  final <- reactive({
+    result <- c()
+    for(i in params_names()){
+      result <- c(result, input[[i]])
+    }
+    if(length(result) > 0){
+      final <- eval(parse(text = paste(input$func,"(",paste0(result,collapse=","),")")))
+    }
+    final
+  })
+  
+  output$discount <- renderPlot({
+      plot(final(),type="discount")
+      })
+  
+  output$posteriors <- renderPlot({
+    plot(final(),type="posteriors")
+  })
+  
+  output$density <- renderPlot({
+    plot(final(),type="density")
+  })
+  
+  output$summary <- renderPrint({
+    summary(final())
   })
 }
 
