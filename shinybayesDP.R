587c235e25cb83f28cea5491dee75b452043c78a
Shawn Balcome <balc0022@umn.edu>
2017-04-30 20:49:29 -0500

    Progress...

--- shinybayesDP.R
+++ shinybayesDP.R
@@ -8,12 +8,11 @@ ui <- dashboardPage(title = "bayesDP",
       dashboardHeader(title = "bayesDP"),
       dashboardSidebar(
         shinyjs::useShinyjs(),
-        #shinythemes::themeSelector(),
         tags$head(tags$style(HTML(".sidebar{height:100vh;overflow-y:auto;}"))),
-        actionButton("fishButton", label = "Fish"),
-        hidden(
-          checkboxGroupInput("Check1",label=h4 ("Fish:"), choices = c("Bass","Shark","Tuna"))
-        ),
+        #actionButton("fishButton", label = "Fish"),
+        #hidden(
+        #  checkboxGroupInput("Check1",label=h4 ("Fish:"), choices = c("Bass","Shark","Tuna"))
+        #),
         uiOutput('Button'),
         bookmarkButton(),
         selectInput("func", 
@@ -21,16 +20,18 @@ ui <- dashboardPage(title = "bayesDP",
                     choices = c("bdpnormal", "bdpbinomial", "bdpsurvival"), 
                     selected = "bdpnormal"),
         uiOutput("params"),
+        conditionalPanel(
+          condition = "input.func == 'bdpsurvival'",
         fileInput("file1", "Choose CSV File",
                   accept = c(
                     "text/csv",
                     "text/comma-separated-values,text/plain",
                     ".csv")
-        ),
-        tags$hr(),
-        checkboxInput("header", "Header", TRUE)
+        )),
+        HTML("<br><br><br>")
       ),
       dashboardBody(
+      fluidPage(
         tags$style(type = "text/css",
                    ".shiny-output-error { visibility: hidden; }",
                    ".shiny-output-error:before { visibility: hidden; }"),
@@ -40,13 +41,16 @@ ui <- dashboardPage(title = "bayesDP",
                         tabPanel("posteriors", "posteriors",
                                  plotOutput("posteriors")),
                         tabPanel("density", "density",
-                                 plotOutput("density"))
+                                 plotOutput("density")),
+                        tabPanel("survival", "survival",
+                                 plotOutput("survival"))
         )),
         box(verbatimTextOutput("summary")),
 
-        tags$head(tags$style(HTML("#summary {font-size: 9px;}"))),
-        
-        box(dataTableOutput("contents"))
+        tags$head(tags$style(HTML("#summary {font-size: 8px;}")))
+      ),
+        hr(),
+        dataTableOutput("contents")
       )
     )
 
@@ -70,6 +74,14 @@ server <- function(input, output, enableBookmarking = "url"){
       if(class(params()[[x]]) == "numeric"){
         do.call(textInput,list(x, label = x, value = params()[[x]]))
       }
+      if(class(params()[[x]]) == "formula"){
+        do.call(textInput,
+                list(x, label = x,
+                     value = "Surv(time, status) ~ historical + treatment"))
+      }
+      if(class(params()[[x]]) == "data"){
+        do.call(textInput,list(x, label = x, value = output$contents))
+      }
       if(class(params()[[x]]) == "NULL"){
         do.call(textInput,list(x, label = x, value = sample(1000,1)))
       }
@@ -85,24 +97,24 @@ server <- function(input, output, enableBookmarking = "url"){
       final <- c(final, input[[i]])
     }
     
-    if(input$func == "bdpsurvival"){
-      input$func$data <- output$contents
-    }
-    
     if(length(final) > 0){
-      final <- eval(parse(text = paste0(input$func,"(",
-                                       paste0(final,collapse = ",")
-                                       ,")")))
+      if(input$func == "bdpsurvival"){
+        final <- eval(parse(text = paste0("bdpsurvival",
+                                          "(",
+                                          "Surv(time, status) ~ historical + treatment,",
+                                          "data = survdata(),",
+                                          "fix_alpha = TRUE",
+                                          ")",
+                                          collapse = ",")))
+        print(survdata())
+      }
+      else{
+        final <- eval(parse(text = paste0(input$func,"(",
+                                          paste0(final,collapse = ",")
+                                          ,")")))
+      }
     }
     final
-  })
-  
-  output$contents <- renderTable({
-    inFile <- input$file1
-    if (is.null(inFile)){
-      return(NULL)
-    }
-    read.csv(inFile$datapath, header = input$header)
   })
   
   output$discount <- renderPlot({
@@ -117,19 +129,25 @@ server <- function(input, output, enableBookmarking = "url"){
     plot(final(),type = "density")
   })
   
+  output$survival <- renderPlot({
+    plot(final())
+  })
+
   output$summary <- renderPrint({
     summary(final())
   })
   
-  output$contents <- renderDataTable({
+  survdata <- reactive({
     inFile <- input$file1
     if (is.null(inFile)){
       return(NULL)
     }
-    read.csv(inFile$datapath, header = input$header)
+    read.csv(inFile$datapath,header=TRUE)
   })
   
-  observeEvent(input$fishButton,{toggle("Check1")})
+  output$contents <- renderDataTable({survdata()})
+  
+  #observeEvent(input$fishButton,{toggle("Check1")})
 }
   
 shinyApp(ui, server)
